e37c8f66e5abf042ba29cbe61f73b58d63553fb9
VicoMaster <job.devcode@gmail.com>
2021-11-02 04:35:49 -0500

    fix whatsapp

--- js/go-top.js
+++ js/go-top.js
@@ -1,8 +1,10 @@
 window.onscroll = function () {
     if (document.documentElement.scrollTop > 100) {
         document.querySelector('.go-top-container').classList.add('show');
+        document.querySelector('.container-button-whatsapp').classList.add('show-whatsapp');
     } else {
         document.querySelector('.go-top-container').classList.remove('show');
+        document.querySelector('.container-button-whatsapp').classList.remove('show-whatsapp');
     }
 }
 
