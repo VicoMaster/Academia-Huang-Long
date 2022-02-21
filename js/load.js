cbbbf5db29be904963b27bb68e8e0af0304926da
VicoMaster <job.devcode@gmail.com>
2022-02-21 02:47:21 -0500

    Load Max 6 seg

--- js/load.js
+++ js/load.js
@@ -1,5 +1,19 @@
+let expireTime = setInterval(hiddenLoadDragon,1000);
+let conteo = 0; //Cuando el conteo llegue a 6 cancela el logo de carga
+
 window.onload = () => {
     pantalla_carga = document.getElementById('overlay-load');
     pantalla_carga.style.visibility = 'hidden';
     pantalla_carga.style.opacity = '0';
+}
+
+function hiddenLoadDragon(){
+    conteo++
+    if(conteo > 6){
+        pantalla_carga = document.getElementById('overlay-load');
+        pantalla_carga.style.visibility = 'hidden';
+        pantalla_carga.style.opacity = '0';  
+        clearInterval(expireTime);
+    }
+    console.log("CONTEO: ",conteo);
 }
